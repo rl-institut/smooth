991be9b8f6266a534459313505b9bf99c54572bb
Marlon Fleck <marlon.fleck@rl-institut.local>
2019-10-15 15:39:21 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='smooth',
+    version='0.1dev',
+    packages=['smooth',],
+    license='GNU AFFERO GENERAL PUBLIC LICENSE - Version 3, 19 November 2007',
+    long_description=open('README.md').read(),
+)
