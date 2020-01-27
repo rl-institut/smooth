76efe6dbd52473c2fe905ac3c12f6b86c05ed511
Jan Timo Meyer <Timo.Meyer@rl-institut.de>
2020-01-27 11:42:07 +0100

    Add emissions update function to specific components

--- smooth/components/component_fuel_cell_chp.py
+++ smooth/components/component_fuel_cell_chp.py
@@ -164,3 +164,11 @@ class FuelCellChp(Component):
         this_energy_supplied_electric = self.flows[flow_name[1]][sim_params.i_interval]
         # Call the function of the mother component to save costs and art. costs for this run.
         Component.update_costs(self, results, sim_params, this_energy_supplied_electric)
+
+    def update_var_emissions(self, results, sim_params):
+        # Get the name of the flow of this component.
+        flow_name = list(self.flows)
+        # Get the amount of electric energy supplied by the chp this interval time step [Wh].
+        this_energy_supplied_electric = self.flows[flow_name[1]][sim_params.i_interval]
+        # Call the function of the mother component to save emissions for this run.
+        Component.update_var_emissions(self, results, sim_params, this_energy_supplied_electric)
