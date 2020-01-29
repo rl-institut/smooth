8f6fe210c125c32a9889314184aab0260fce07fe
Jan Timo Meyer <Timo.Meyer@rl-institut.de>
2020-01-29 18:25:54 +0100

    Do not print annuities if optimization was not successful

--- smooth/examples/run_smooth_example.py
+++ smooth/examples/run_smooth_example.py
@@ -5,6 +5,8 @@ from smooth import save_results
 from smooth import plot_smooth_results
 
 # Run an example.
-smooth_result = run_smooth(mymodel)
-print_smooth_results(smooth_result)
+smooth_result, status = run_smooth(mymodel)
 plot_smooth_results(smooth_result)
+if status == "ok":
+    print_smooth_results(smooth_result)
+
