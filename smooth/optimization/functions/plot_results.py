936fb53a5e5c1fb2cf4104e4f1d212ae03833605
stefan.schirmeister <stefan.schirmeister@localiser.de>
2020-04-02 15:08:57 +0200

    plot_optimization: template for scatter plot

--- smooth/optimization/functions/plot_results.py
+++ smooth/optimization/functions/plot_results.py
@@ -1,19 +1,26 @@
 # PLOT RESULTS
 def plot_optimization_results(optimization_results):
     # Plot the progress.
-    import seaborn as sns
     import matplotlib.pyplot as plt
 
     stats = optimization_results.stats
-
-    sns.set()
 
     # _ = plt.scatter(range(1, len(stats) + 1), [[s['mu'], s['min'], s['max']] for s in stats], marker='.')
     plt.plot([s['mu'] for s in stats], '.b')
     plt.plot([s['min'] for s in stats], '-g')
 
-    _ = plt.title('average fitness per iteration')
-    _ = plt.xlabel('generation')
-    _ = plt.ylabel('fitness')
+    plt.title('average fitness per iteration')
+    plt.xlabel('generation')
+    plt.ylabel('fitness')
+
+    # cmap = plt.cm.get_cmap("viridis")
+    # cmap.set_bad(color="r")
+    # plt.scatter(
+        # [r[0] for r in result],
+        # [r[1] for r in result],
+        # c=[r[2] or float("inf") for r in result],
+        # cmap=cmap,
+        # plotnonfinite=True
+    # )
 
     plt.show()
