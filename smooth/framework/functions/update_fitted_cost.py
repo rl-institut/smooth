1ea7747b9f87d1a29cb12eee24bb4ee4d31ed8a4
jtm91 <jtm91@mailbox.tu-berlin.de>
2020-04-07 16:38:05 +0200

    Catch dependant value being none

--- smooth/framework/functions/update_fitted_cost.py
+++ smooth/framework/functions/update_fitted_cost.py
@@ -176,7 +176,11 @@ def get_free(component, fitting_dict, index, dependant_value):
 
 def get_dependant_value(component, fitting_dict, index, fixedCost):
     # Get an attribute of the component as the dependant value.
-    dependant_value = getattr(component, fitting_dict['dependant_value'][index])
+    try:
+        dependant_value = getattr(component, fitting_dict['dependant_value'][index])
+    except:
+        dependant_value = None
+
     if fitting_dict['dependant_value'][index] == fixedCost:
         # If the capex are chosen as the dependant value, the capex costs are meant.
         dependant_value = dependant_value['cost']
