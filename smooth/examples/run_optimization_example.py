c285bb9e02df19a9af98accef72eb9f482141e60
stefan.schirmeister <stefan.schirmeister@localiser.de>
2020-04-09 16:24:32 +0200

    Add comment to new optimization parameter 'plot_progress'

--- smooth/examples/run_optimization_example.py
+++ smooth/examples/run_optimization_example.py
@@ -15,15 +15,7 @@ def main():
     #  pop_size: Number of individuals in the population [-].
     #  n_gen: Number of generations that will be evaluated [-].
     #  n_core: Number of cores used in the optimization ('max' will use all of them) [-].
-    # generation: dictionary with relative number of individuals to select, crossover and mutate in each generation.
-    # With a population_size of 30, a 2/3/5 generation:
-    # - selects 6 best individuals without change
-    # - generates at most 9 children through crossover of these 6 selected
-    # - generates at least 15 children through mutation of the 6 selected
-    # weights: tuple controlling fitness function of optimization.
-    # negative numbers minimize, the absolute value describes relative importance.
-    # First value is linked to cost, the second to emissions.
-    # (-1, -1) means minimze costs and emissions, with equal importance.
+    #  plot_progress: show parteo front during each stop of the simulation [False].
     opt_params['ga_params'] = {
         'population_size': 30,
         'n_generation': 15,
