f4c7cdf188f765acfbc45981292ba8695e9d133e
Ciara Dunks <ciara.dunks@rl-institut.de>
2020-04-30 10:03:04 +0200

    new english dicts implemented

--- smooth/examples/example_plotting_dicts.py
+++ smooth/examples/example_plotting_dicts.py
@@ -1,4 +1,6 @@
-comp_dict = {
+# ------------------- GERMAN DICTIONARIES -------------------
+
+comp_dict_german = {
     'this_ely': 'Elektrolyseur',
     'this_pem_ely': 'PEM-Elektrolyseur',
     'pv_output': 'PV-Anlage',
@@ -20,7 +22,7 @@ comp_dict = {
     'wind_to_grid': 'Überschüssige Wind-Elektrizität',
 }
 
-bus_dict = {
+bus_dict_german = {
     'bel': 'Elektrische Energie',
     'bel_wind':	'Wind Energie',
     'bel_pv': 'PV Energie',
@@ -31,7 +33,7 @@ bus_dict = {
     'bch4': 'Biomethan-Fluss',
 }
 
-y_dict = {
+y_dict_german = {
     'bel': 'Energie in Wh',
     'bel_wind':	'Energie in Wh',
     'bel_pv': 'Energie in Wh',
@@ -40,4 +42,28 @@ y_dict = {
     'bh2_mp': 'Wasserstoff in kg',
     'bh2_hp': 'Wasserstoff in kg',
     'bch4': 'Biomethan in kg',
+}
+
+# ------------------- ENGLISH DICTIONARIES -------------------
+
+comp_dict_german = {
+    'this_ely': 'Elektrolyseur',
+    'this_pem_ely': 'PEM-Elektrolyseur',
+    'pv_output': 'PV-Anlage',
+    'solar_output': 'PV-Anlage',
+    'wind_output': 'WE-Anlage',
+    'th_demand': 'Heizbedarf',
+    'h2_demand': 'Wasserstoffbedarf',
+    'h2_compressor': 'Wasserstoffkompressor (higher pressure)',
+    'from_grid': 'Strombezug',
+    'to_grid': 'Stromeinspeisung',
+    'h2_storage': 'Wasserstoffspeicher',
+    'fuel_cell_chp': 'Brennstoffzelle',
+    'CHP_Methane': 'Biogas-BHKW',
+    'ch4_grid': 'Biogas-Zufuhr',
+    'h2_compressor_from_ely': 'Wasserstoffkompressor (lower pressure)',
+    'dummy_2': 'Gebrauchte PV-Elektrizität',
+    'dummy_1': 'Gebrauchte Wind-Elektrizität',
+    'pv_to_grid': 'Überschüssige PV-Elektrizität',
+    'wind_to_grid': 'Überschüssige Wind-Elektrizität',
 }
