bfdc28706fa4d6aed7ec99be815105b872368a7c
Ciara Dunks <ciara.dunks@rl-institut.de>
2020-05-05 14:07:48 +0200

    added docstrings for readthedocs

--- smooth/components/component.py
+++ smooth/components/component.py
@@ -4,52 +4,92 @@ from smooth.framework.functions.update_annuities import update_annuities
 
 
 class Component:
+    """The generic component class is the mother class for all of the components. The parameters and
+    functions defined here can be applied to each of the specific components.
 
+    Parameters
+    ----------
+    param component: The component type
+    type component: str
+    param name: The specific name of the component (must be different to other component names in
+    system)
+    type name: str
+    param life_time: The lifetime of the component [a]
+    type life_time: float
+    param sim_params: The simulation parameters such as the interval time and interest rate
+    type sim_params: object ToDo: check this
+
+    param results: The dictionary containing the main results for the component
+    type results: dict
+    param states: The dictionary containing the varying states for the component
+    type states: dict
+
+    param variable_costs: The variable costs of the component [EUR/*]
+    type variable_costs: float
+    param artificial_costs: The artificial costs of the component [EUR/*] (Note: these costs are
+    not included in the final financial analysis)
+    param artificial_costs: float
+    param dependency_flow_costs: The flow that the costs are dependent on
+    type dependency_flow_costs: str ToDo: maybe this changes with Timo's new pull request?
+
+    param capex: The capital costs ToDo: would the explanation of how CAPEX and OPEX works be described here?
+    type capex: dict
+    param opex: The operational and maintenance costs
+    type opex: dict
+
+    param variable_emissions: The variable emissions of the component [kg/*]
+    type variable_emissions: float
+    param dependency_flow_emissions: The flow that the emissions are dependent on
+    type dependency_flow_emissions: str ToDo: maybe this changes with Timo's new pull request?
+    param op_emissions: The operational emission values
+    type op_emissions: dict
+    param fix_emissions: The fixed emission values
+    type fix_emissions: dict
+
+    param fs_component_name: The foreign state component name
+    type fs_component_name: str
+    param fs_attribute_name: The foreign state attribute name
+    type fs_attribute_name: str
+
+    Notes
+    -----
+    ToDo: maybe CAPEX/OPEX explanations here? Or outside of component description
+    """
     def __init__(self):
-        # PARAMETERS
-        # Define the component type (e.g. "electrolyzer" means component_electrolyzer.py)
+        """Constructor method
+        """
         self.component = None
-        # Define a name (needs to different from the other names of components
-        # in this energy system).
         self.name = None
-        # Life time [a].
         self.life_time = None
-        # Simulation parameters, like interval time and interest rate.
         self.sim_params = None
-
-        # INITIALIZE VARIABLES EACH COMPONENT WILL HAVE.
-        # Initializing results and states as empty dicts.
         self.results = {}
         self.states = {}
-
-        # VARIABLE COSTS
-        # Initializing variable cost and art. cost values [EUR/*].
         self.variable_costs = None
         self.artificial_costs = None
         self.dependency_flow_costs = None
-
-        # FINANCIALS (CAPEX AND OPEX)
         self.opex = dict()
         self.capex = dict()
-
-        # Initializing variable emission values [kg/*] and the flow [*] it depends on.
         self.variable_emissions = None
         self.dependency_flow_emissions = None
-        # Initializing fixed and operational emission values.
         self.op_emissions = dict()
         self.fix_emissions = dict()
-
-        # FOREIGN STATES
-        # Initializing foreign state component name and attribute name, if set
-        # both need to be strings.
         self.fs_component_name = None
         self.fs_attribute_name = None
 
+    # ------------------- SET THE PARAMETERS FOR EACH COMPONENT -------------------
+
     def set_parameters(self, params):
+        """Sets the parameters that have been defined by the user (in the model definition) in
+        the necessary components, overwriting the default parameter values. Errors are raised if:
+        - the given parameter is not part of the component (interval time is an exception)
+        - the dependency flows have not been defined
+
+        :param params: The set of parameters defined in the specific component class
+        :type params: dict ToDo: make sure of this, maybe list
+        :return: Potential raised errors
+        """
         for this_param in params:
             if not hasattr(self, this_param):
-                # Raise an error if the given parameter is not part of the
-                # component (interval time is an exception).
                 raise ValueError(
                     'The parameter "{}" is not part of the component'.format(this_param))
 
@@ -67,12 +107,21 @@ class Component:
     # ------------------- UPDATE THE FLOWS FOR EACH COMPONENT -------------------
 
     def update_flows(self, results, sim_params, comp_name=None):
+        """Updates the flows of a component for each time step.
+
+        :param results: The oemof results for the given time step
+        :type results: object
+        :param sim_params: The simulation parameters for the energy system (defined by user)
+        :type sim_params: object
+        :param comp_name: The name of the component - while components can generate
+        more than one oemof model, they sometimes need to give a custom name, defaults to None
+        :type comp_name: str, optional
+        :return: updated flow values for each flow in the 'flows' dict
+        """
         # Check if the component has an attribute 'flows', if not, create it as an empty dict.
         if not hasattr(self, 'flows'):
             self.flows = {}
 
-        # While components can generate more than one oemof model, they
-        # sometimes need to give a custom name.
         if comp_name is None:
             comp_name = self.name
 
@@ -92,27 +141,62 @@ class Component:
     # ------------------- PREPARE CREATING THE OEMOF MODEL -------------------
 
     def prepare_simulation(self, components):
-        # If a component has artificial costs, this update_artificial_costs
-        # function is overwritten in that component
+        """Prepares the simulation. If a component has artificial costs, this
+        prepare_simulation function is overwritten in the specific component.
+
+        :param components: List containing each component object
+        :type components: list
+        :return: If used as a placeholder, nothing will be returned. Else, refer to
+        specific component that uses the prepare_simulation function for further detail.
+        """
         pass
 
-    # ------ UPDATE STATES (PLACEHOLDER FOR COMPONENTS WITHOUT STATES) ------
+    # ------------------- UPDATE STATES -------------------
 
     def update_states(self, results, sim_params):
-        # If a component has states, this update_states function is overwritten in that component
+        """Updates the states, used as placeholder for components without states.
+        If a component has states, this update_states function is overwritten in the
+        specific component.
+
+        :param results: The oemof results object for the given time step
+        :type results: object
+        :param sim_params: The simulation parameters for the energy system (defined by user)
+        :type sim_params: object
+        :return: If used as a placeholder, nothing will be returned. Else, refer to
+        specific component that uses the update_states function for further detail.
+        """
         pass
 
+    # -------------- UPDATE CONSTRAINTS (PLACEHOLDER) --------------
+
     def update_constraints(self, busses, model_to_solve):
-        # Sometimes special contraints are needed, these can be written here.
+        """Sometimes special constraints are required for the specific components,
+        which can be written here. Else, this function is used as placeholder for
+        components without constraints.
+
+        :param busses: The virtual buses used in the energy system
+        :type busses: list
+        :param model_to_solve: ToDo: look this up in oemof
+        :type model_to_solve:
+        :return: If used as a placeholder, nothing will be returned. Else, refer to
+        specific component that uses the update_constraints function for further detail.
+        """
         pass
 
     # ------------------- UPDATE THE COSTS -------------------
 
     def update_var_costs(self, results, sim_params):
-        # Track the costs and artificial costs of a component for each time step.
-        # Parameters:
-        #  results: oemof result object for this time step.
-        #  sim_params: simulation parameters defined by the user.
+        """
+        Tracks the cost and artificial costs of a component for each time step.
+
+        :param results: The oemof results object for the given time step
+        :type results: object
+        :param sim_params: The simulation parameters for the energy system (defined by user)
+        :type sim_params: object
+        :return: New values for the updated variable and artificial costs stored in
+        results['variable_costs'] and results['art_costs'] respectively
+        """
+
 
         # First create an empty cost and art. cost array for this component, if
         # it hasn't been created before.
@@ -135,11 +219,14 @@ class Component:
                 self.artificial_costs
 
     def update_var_emissions(self, results, sim_params):
-        # Track the emissions of a component for each time step.
-        # Parameters:
-        #  results: oemof result object for this time step.
-        #  sim_params: simulation parameters defined by the user.
+        """Tracks the emissions of a component for each time step.
 
+        :param results: The oemof results object for the given time step
+        :type results: object
+        :param sim_params: The simulation parameters for the energy system (defined by user)
+        :type sim_params: object
+        :return: A new value for the updated emissions stored in results['variable_emissions']
+        """
         # First create an empty emission array for this component, if it hasn't been created before.
         if 'variable_emissions' not in self.results:
             # If this function is not overwritten in the component, then
@@ -157,7 +244,10 @@ class Component:
     # ------ ADD COSTS AND ARTIFICIAL COSTS TO A PARAMETER IF THEY ARE NOT NONE ------
 
     def get_costs_and_art_costs(self):
-        # Initialize the total variable costs and art. costs [EUR/???].
+        """Initialize the total variable costs and art. costs [EUR/*]
+
+        :return: the total variable costs (including artificial costs)
+        """
         variable_costs_total = 0
         # Add costs and art. costs to an attribute
         if self.variable_costs is not None:
@@ -168,15 +258,17 @@ class Component:
         return variable_costs_total
 
     def get_foreign_state_value(self, components, index=None):
-        # Get a foreign state attribute value with the name fs_attribute_name
-        # of the component fs_component_name. If the fs_component_name is None
-        # and the fs_attribute_name set to a number, the number is given back
-        # instead.
-        # Parameters:
-        #  components: List containing each component object.
-        #  index: Index of the foreign state (should be None if there is only
-        #  one foreign state) [-].
+        """ Get a foreign state attribute value with the name fs_attribute_name
+        of the component fs_component_name. If the fs_component_name is None
+        and the fs_attribute_name set to a number, the number is given back instead.
 
+        :param components: List containing each component object
+        :type components: object
+        :param index: Index of the foreign state (should be None if there is only
+        one foreign state) [-]
+        :type index: int, optional
+        :return: Foreign state value returned
+        """
         if index is None:
             fs_component_name = self.fs_component_name
             fs_attribute_name = self.fs_attribute_name
@@ -207,8 +299,10 @@ class Component:
         return foreign_state_value
 
     def generate_results(self):
-        # Generate the results after the simulation.
+        """Generates the results after the simulation.
 
+        :return: Results for the computed emissions, financials and annuities
+        """
         # Compute the emissions due to installation and operation.
         update_emissions(self, self.fix_emissions)
         update_emissions(self, self.op_emissions)
@@ -219,9 +313,11 @@ class Component:
         update_annuities(self)
 
     def check_validity(self):
-        # This function is called immediately after the component object is
-        # created and checks if the component attributes are valid.
+        """This function is called immediately after the component object is created and
+        checks if the component attributes are valid.
 
+        :return: Potential errors raised
+        """
         # Check if a life time is given when there are CAPEX given.
         if self.capex or self.fix_emissions:
             if self.life_time is None or self.life_time <= 0:
