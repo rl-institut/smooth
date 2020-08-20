25ed9fd9564803b08e630f0f42cef7441b1366f2
Ciara Dunks <ciara.dunks@rl-institut.de>
2020-08-20 13:10:43 +0200

    minor additions to external components and example

--- smooth/examples/run_smooth_example.py
+++ smooth/examples/run_smooth_example.py
@@ -1,7 +1,8 @@
-from smooth.examples.example_model import mymodel
+from smooth.examples.example_model_external_components import mymodel
 from smooth import run_smooth
 from smooth import plot_smooth_results
 from smooth import print_smooth_results
+from smooth import save_results
 from smooth.examples.example_plotting_dicts import comp_dict_german
 
 if __name__ == '__main__':
@@ -9,3 +10,4 @@ if __name__ == '__main__':
     smooth_result, status = run_smooth(mymodel)
     plot_smooth_results(smooth_result, comp_dict_german)
     print_smooth_results(smooth_result)
+    save_results('external_components_test', smooth_result)
