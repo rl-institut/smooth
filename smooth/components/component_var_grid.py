bf1950a4251de87f5aa4dc50df074cf389c879c7
Julian Brendel <julian.brendel@rl-institut.de>
2020-08-21 17:57:57 +0200

    rewrite component as child(battery)

--- smooth/components/component_var_grid.py
+++ smooth/components/component_var_grid.py
@@ -1,8 +1,7 @@
-import oemof.solph as solph
-from .component import Component
+from smooth.components.component_supply import Supply
 
 
-class VarGrid(Component):
+class VarGrid(Supply):
     """ An electric grid with different connection levels can be created through this class
 
         Gridlevels [1,2,3,4,5,6] describe the grid connection, if left unmodified these are:
@@ -25,13 +24,7 @@ class VarGrid(Component):
     """
 
     def __init__(self, params):
-
-        # Call the init function of the mother class.
-        Component.__init__(self)
-
         # ------------------- PARAMETERS -------------------
-        self.name = 'Variable_Grid_default_name'
-
         # Gridlevels [1,2,3,4,5,6] describe the grid connection:
         # [1] house connection, [2] lov voltage grid, [3] lov voltage local network station
         # [4] medium voltage grid, [5] medium voltage transformer station, [6] High Voltage
@@ -81,23 +74,17 @@ class VarGrid(Component):
             'fitting_value': [[10000000, 0.01]],
             'dependant_value': ['output_max']}
 
-        self.bus_out = None
-
-        # ------------- PARAMETERS ARTIFICIAL COSTS FOREIGN STATE --------------
-        # The artificial costs for supplying electricity can be dependant on a
-        # foreign state, like a storage SoC. Therefore the name and the state
-        # name of that foreign entity have to be defined as well as the threshold
-        # level, under which the low level costs are used. Above the threshold,
-        # the high level artificial costs are used.
-
-        # Define the threshold value for the artificial costs.
-        self.fs_threshold = None
-        # Define the low and the high art. cost value e.g. [EUR/Wh], [EUR/kg]
-        self.fs_low_art_cost = None
-        self.fs_high_art_cost = None
+        # Extract params unique in child class (VarGrid) from params and call
+        # mother class (Supply) init with the general supply params
+        params_child = {}
+        for key in self.__dict__.keys():
+            if key in params:
+                params_child[key] = params.pop(key)
 
         # ------------------- UPDATE PARAMETER DEFAULT VALUES -------------------
-        self.set_parameters(params)
+        Supply.__init__(self, params)  # initialize and update "supply" parameters
+        self.set_parameters(params_child)  # update parameters unique to "VarGrid"
+
         # adjust to set level of grid and given timestep
         if self.grid_level == 1:
             self.output_max = self.grid_l1_output_max
@@ -117,29 +104,3 @@ class VarGrid(Component):
         elif self.grid_level == 6:
             self.output_max = self.grid_l6_output_max
             self.capex = self.capex_l6
-
-        # ------------------- INTERNAL VALUES -------------------
-        # The current artificial cost value e.g. [EUR/Wh], [EUR/kg].
-        self.current_ac = 0
-
-    def prepare_simulation(self, components):
-        # Update the artificial costs for this time step (dependant on foreign states).
-        if self.fs_component_name is not None:
-            foreign_state_value = self.get_foreign_state_value(components)
-            if foreign_state_value < self.fs_threshold:
-                self.artificial_costs = self.fs_low_art_cost
-            else:
-                self.artificial_costs = self.fs_high_art_cost
-
-        # Set the total costs for the commodity this time step
-        # (costs + art.  costs) e.g. [EUR/Wh], [EUR/kg].
-        self.current_ac = self.get_costs_and_art_costs()
-
-    def create_oemof_model(self, busses, _):
-        from_grid = solph.Source(
-            label=self.name,
-            outputs={busses[self.bus_out]: solph.Flow(
-                nominal_value=self.output_max,
-                variable_costs=self.current_ac
-            )})
-        return from_grid
