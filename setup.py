5e05f2ca5de4dcf63b1d427a2e3f5378bf6b3552
Johannes Hofmann <mail@b-r-u.org>
2020-09-01 16:47:30 +0200

    Add classifiers to setup.py
    This documents the licenses, development status, intended audience, etc.
    of this library.

--- setup.py
+++ setup.py
@@ -8,4 +8,13 @@ setup(
     package_data={'smooth.examples': ['example_timeseries/*.csv']},
     license='Dual-License MIT/Apache-2.0',
     long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
 )
