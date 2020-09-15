cc2fb5dc0e2669a1d4e0063f7b7310ef598e94d7
Johannes Hofmann <mail@b-r-u.org>
2020-09-15 14:39:20 +0200

    requirements: Restrict version of numpy and pandas

--- setup.py
+++ setup.py
@@ -8,4 +8,13 @@ setup(
     package_data={'smooth.examples': ['example_timeseries/*.csv']},
     license='GNU AFFERO GENERAL PUBLIC LICENSE - Version 3, 19 November 2007',
     long_description=open('README.md').read(),
+    #install_requires=[
+        #'oemof@git+git://github.com/oemof/oemof-solph@585b123e3dc02b191fead4d202ba60c057c473fd',
+        #'matplotlib',
+        #'pandas<0.26',
+        #'numpy<1.18',
+        #'scipy',
+        #'dill',
+        #'pyutilib==5.8.0',
+    #]
 )
