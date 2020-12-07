ac9c8ee9768e8c5a2d526dc4ac331d85b816d943
Julian Brendel <Julian.Brendel@rl-institut.de>
2020-12-07 16:55:23 +0100

    update example_model to comply with #210

--- smooth/examples/example_model.py
+++ smooth/examples/example_model.py
@@ -77,21 +77,24 @@ components.append({
     'life_time': 50,
     'capex': {
         'key': 'variable',
-        'var_capex_dependency': 'output_max',
-        0: {
-            'low_threshold': 0,
-            'high_threshold': 3000000,
-            'key': 'spec',
-            'fitting_value': 0.2,
-            'dependant_value': 'output_max',
-        },
-        1: {
-            'low_threshold': 3000000,
-            'high_threshold': float('inf'),
-            'key': 'spec',
-            'fitting_value': 0.1,
-            'dependant_value': 'output_max',
-        },
+        'var_dict_dependency': 'output_max',
+        'var_dicts':
+        [
+            {
+                'low_threshold': 0,
+                'high_threshold': 3000000,
+                'key': 'spec',
+                'fitting_value': 0.2,
+                'dependant_value': 'output_max',
+            },
+            {
+                'low_threshold': 3000000,
+                'high_threshold': float('inf'),
+                'key': 'spec',
+                'fitting_value': 0.1,
+                'dependant_value': 'output_max',
+            },
+        ]
     },
 })
 
